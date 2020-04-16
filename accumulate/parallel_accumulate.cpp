d3d9b4ac3e10377429172b2c728afabb19a29d64
rogovrt <49844050+rogovrt@users.noreply.github.com>
2020-04-16 21:45:07 +0300

    Uploaded accumulate

---
+++ accumulate/parallel_accumulate.cpp
@@ -0,0 +1,109 @@
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <thread>
+#include <vector>
+#include <chrono>
+#include <fstream>
+#include <iomanip>
+#include <functional>
+
+class Timer {
+public:
+	Timer() : m_begin(std::chrono::steady_clock::now()), duration(0), is_work(true) {}
+
+	~Timer() {
+		if (is_work)
+			this->stop();
+		//fout << duration << " milliseconds" << std::endl;
+	}
+
+	int stop() {
+		auto end = std::chrono::steady_clock::now();
+		duration += std::chrono::duration_cast<std::chrono::milliseconds> (end - m_begin).count();
+		is_work = false;
+		return duration;
+	}
+
+	void go() {
+		m_begin = std::chrono::steady_clock::now();
+		is_work = true;
+	}
+
+private:
+	std::chrono::steady_clock::time_point m_begin;
+	int duration;
+	bool is_work;
+};
+
+template < typename Iterator, typename T >
+struct accumulate_block
+{
+	void operator()(Iterator first, Iterator last, T& result)
+	{
+		result = std::accumulate(first, last, result); // !
+	}
+};
+
+template < typename Iterator, typename T >
+T parallel_accumulate(Iterator first, Iterator last, T init, std::size_t num_threads)
+{
+	const std::size_t length = std::distance(first, last);
+
+	if (!length)
+		return init;
+
+	const std::size_t min_per_thread = 25;
+	const std::size_t max_threads =
+		(length + min_per_thread - 1) / min_per_thread;
+	/*
+	const std::size_t hardware_threads =
+		std::thread::hardware_concurrency();
+
+	const std::size_t num_threads =
+		std::min(hardware_threads != 0 ? hardware_threads : 2, max_threads);
+	*/
+	const std::size_t block_size = length / num_threads;
+
+	std::vector < T > results(num_threads);
+	std::vector < std::thread > threads(num_threads - 1);
+
+	Iterator block_start = first;
+
+	for (std::size_t i = 0; i < (num_threads - 1); ++i)
+	{
+		Iterator block_end = block_start;
+		std::advance(block_end, block_size);
+
+		threads[i] = std::thread(
+			accumulate_block < Iterator, T >(),
+			block_start, block_end, std::ref(results[i])); // !
+
+		block_start = block_end;
+	}
+
+	accumulate_block < Iterator, T >()(block_start, last, results[num_threads - 1]);
+
+	std::for_each(threads.begin(), threads.end(),
+		std::mem_fn(&std::thread::join));
+
+	return std::accumulate(results.begin(), results.end(), init);
+}
+
+int main(int argc, char** argv)
+{
+	std::ofstream fout("./result.txt");
+	std::vector < double > v(180000000);
+	std::iota(v.begin(), v.end(), 1.0);
+	for (std::size_t i = 2; i <= 11; ++i) {
+		//fout << std::setw(10) << i;
+		Timer t;
+		parallel_accumulate(v.begin(), v.end(), 0, i);
+		//fout << std::setw(10) << t.stop();
+		fout << t.stop();
+		fout << std::endl;
+	}
+	system("pause");
+
+	return EXIT_SUCCESS;
+}
